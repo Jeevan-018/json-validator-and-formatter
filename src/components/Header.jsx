fcbbf250046712540e5e1d7cc975463ef46ceb7a
vidyashreeraju <vidyashreesraju@gmail.com>
2025-07-01 19:49:44 +0530

    OK

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ export default function Header() {
   return (
     <header className="header">
       <div className="logo">JSON Validator & Formatter</div>
+      <h1>Vidyashree</h1>
     </header>
   );
 }
