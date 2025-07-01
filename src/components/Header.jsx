d849999d86fefcd64916699c8c1a47b060090581
Ranjitha <ranjithamr44@gmail.com>
2025-07-01 20:12:54 +0530

    OK

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ export default function Header() {
   return (
     <header className="header">
       <div className="logo">JSON Validator & Formatter</div>
+      <h1>Ranjitha M R</h1>
     </header>
   );
 }
